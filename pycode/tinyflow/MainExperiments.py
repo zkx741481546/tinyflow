d737e9580988aaafa692c11de4aaa5b8a38cd773
zkx741481546 <zkx741481546@live.com>
2021-06-23 16:22:49 +0800

    实验代码 v1.13.1 修改了一些配置，实验代码

--- pycode/tinyflow/MainExperiments.py
+++ pycode/tinyflow/MainExperiments.py
@@ -4,9 +4,9 @@ from Inceptionv4_test_leo import run_exp as InceptionV4
 from ResNet50_test_leo import run_exp as ResNet50
 from DenseNet_test_leo import run_exp as DenseNet
 
-# VGG([['./log/VGG/', 3, 1, 16], ['./log/VGG x1/', 3, 1, 2], ['./log/VGG x2/', 3, 2, 2], ['./log/VGG x3/', 3, 3, 2]])
-# InceptionV3([['./log/Inception V3 x2/', 3, 2, 2], ['./log/Inception V3 x3/', 3, 3, 2]])
-# InceptionV4([['./log/Inception V4/', 3, 1, 16], ['./log/Inception V4 x1/', 3, 1, 2], ['./log/Inception V4 x2/', 3, 2, 2], ['./log/Inception V4 x3/', 3, 3, 2]])
-ResNet50([['./log/ResNet x1/', 3, 1, 2]])
-# ResNet50([['./log/ResNet/', 3, 1, 16], ['./log/ResNet x1/', 3, 1, 2], ['./log/ResNet x2/', 3, 2, 2], ['./log/ResNet x3/', 3, 3, 2]])
-# DenseNet([['./log/DenseNet/', 3, 1, 16], ['./log/DenseNet x1/', 3, 1, 2], ['./log/DenseNet x2/', 3, 2, 2], ['./log/DenseNet x3/', 3, 3, 2]])
+VGG([['./log/VGG/', 3, 1, 16], ['./log/VGG x1/', 3, 1, 2], ['./log/VGG x2/', 3, 2, 2], ['./log/VGG x3/', 3, 3, 2]])
+InceptionV3([['./log/Inception V3 x2/', 3, 2, 2], ['./log/Inception V3 x3/', 3, 3, 2]])
+InceptionV4([['./log/Inception V4/', 3, 1, 16], ['./log/Inception V4 x1/', 3, 1, 2], ['./log/Inception V4 x2/', 3, 2, 2], ['./log/Inception V4 x3/', 3, 3, 2]])
+# ResNet50([['./log/ResNet x1/', 3, 1, 2]])
+ResNet50([['./log/ResNet/', 3, 1, 16], ['./log/ResNet x1/', 3, 1, 2], ['./log/ResNet x2/', 3, 2, 2], ['./log/ResNet x3/', 3, 3, 2]])
+DenseNet([['./log/DenseNet/', 3, 1, 16], ['./log/DenseNet x1/', 3, 1, 2], ['./log/DenseNet x2/', 3, 2, 2], ['./log/DenseNet x3/', 3, 3, 2]])
--- pycode/tinyflow/VGG16_test_leo.py
+++ pycode/tinyflow/VGG16_test_leo.py
@@ -204,4 +204,4 @@ def run_exp(workloads):
 
 
 if __name__ == '__main__':
-    run_exp([['./log/VGG passive /', 3, 1, 16], ['./log/VGG passive x1/', 3, 1, 2], ['./log/VGG passive x2/', 3, 2, 2], ['./log/VGG passive x3/', 3, 3, 2]])
+    run_exp([['./log/VGG bs1/', 3, 1, 1]])
